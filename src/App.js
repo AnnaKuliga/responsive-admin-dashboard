19f99c1802c3e0847e3b6fcd3fff778d242d4cb2
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-23 19:05:50 +0200

    created all necessary components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+import Sidebar from "./components/Sidebar Section/Sidebar";
+import Body from "./components/Body section/Body";
+
+function App() {
+  return (
+    <div className="container">
+      <Sidebar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body section/Activity section/Activity.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./activity.css";
+
+const Activity = () => {
+  return <div className="Activity"></div>;
+};
+
+export default Activity;
