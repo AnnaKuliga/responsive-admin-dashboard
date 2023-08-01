dbbbfebc8ca9a3a585760f561e050e52284745f0
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-01 20:02:42 +0200

    added listing items

--- src/components/Body section/Listing section/Listing.jsx
+++ src/components/Body section/Listing section/Listing.jsx
@@ -7,6 +7,10 @@ import { AiFillHeart } from "react-icons/ai";
 
 //Imported images ====>
 import coffee_seeds from "../../../assets/coffee/coffee_seeds.png";
+import coffee_seeds3 from "../../../assets/coffee/coffee-seeds3.png";
+import coffee_maker from "../../../assets/coffee/Coffeemaker French Presses.png";
+import coffee_maker2 from "../../../assets/coffee/Krups Nescafe Dolce Gusto coffee mahcine.png";
+
 const Listing = () => {
   return (
     <div className="listingSection">
@@ -22,6 +26,31 @@ const Listing = () => {
           <AiFillHeart className="icon" />
           <img src={coffee_seeds} alt="coffee seeds" />
           <h3>Coffee seeds</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_seeds3} alt="coffee seeds" />
+          <h3>
+            Coffee seeds <br />
+            <small> choose the flavour</small>
+          </h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_maker} alt="coffee maker" />
+          <h3>
+            Coffeemaker <br /> <small>French Presses</small>
+          </h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_maker2} alt="coffee maker" />
+          <h3>
+            Krups Nescafe <br /> <small>Dolce Gusto</small>
+          </h3>
         </div>
       </div>
     </div>
