435f187697f65d1f9625a482ca48d23fba5b274f
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-07 18:39:11 +0200

    Added extra listing items

--- src/components/Body section/Listing section/Listing.jsx
+++ src/components/Body section/Listing section/Listing.jsx
@@ -8,8 +8,13 @@ import { AiFillHeart } from "react-icons/ai";
 //Imported images ====>
 import coffee_seeds from "../../../assets/coffee/coffee_seeds.png";
 import coffee_seeds3 from "../../../assets/coffee/coffee-seeds3.png";
+import coffee_seeds2 from "../../../assets/coffee/coffee_seeds2.png";
 import coffee_maker from "../../../assets/coffee/Coffeemaker French Presses.png";
 import coffee_maker2 from "../../../assets/coffee/Krups Nescafe Dolce Gusto coffee mahcine.png";
+import coffee_maker3 from "../../../assets/coffee/Espresso Machines Coffee Saeco Moka pot.png";
+import coffee_mug from "../../../assets/coffee/coffee_mug.png";
+import coffee_mug2 from "../../../assets/coffee/coffee_mug2.png";
+import coffee_mug3 from "../../../assets/coffee/coffee_mug3.png";
 
 const Listing = () => {
   return (
@@ -26,6 +31,15 @@ const Listing = () => {
           <AiFillHeart className="icon" />
           <img src={coffee_seeds} alt="coffee seeds" />
           <h3>Coffee seeds</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_seeds2} alt="coffee seeds" />
+          <h3>
+            Coffee seeds <br />
+            <small> choose the flavour</small>
+          </h3>
         </div>
 
         <div className="singleItem">
@@ -50,6 +64,36 @@ const Listing = () => {
           <img src={coffee_maker2} alt="coffee maker" />
           <h3>
             Krups Nescafe <br /> <small>Dolce Gusto</small>
+          </h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_maker3} alt="coffee maker" />
+          <h3>Saeco Moka Pot</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_mug} alt="coffee mug" />
+          <h3>
+            Coffee Mug <small>200ml</small>
+          </h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_mug2} alt="coffee mug" />
+          <h3>
+            Coffee Mug <small>300ml</small>
+          </h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={coffee_mug3} alt="coffee mug" />
+          <h3>
+            Coffee Mug <small>200ml</small>
           </h3>
         </div>
       </div>
