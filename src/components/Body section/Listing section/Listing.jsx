fec7685df72f2a083ea4001307e99ff0bb1ca645
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-07 21:22:23 +0200

    added Featured sellers section

--- src/components/Body section/Listing section/Listing.jsx
+++ src/components/Body section/Listing section/Listing.jsx
@@ -115,13 +115,12 @@ const Listing = () => {
 
           <div className="card flex">
             <div className="users">
-              <img src={user1} alt="User Image" />
               <img src={user2} alt="User Image" />
               <img src={user3} alt="User Image" />
             </div>
             <div className="cardText">
               <span>
-                356 bags of Flavoured Ground Coffee <br />
+                456 Flavoured Coffee <br />
                 <small>
                   12 sells within <span className="date">10 Days</span>
                 </small>
@@ -131,14 +130,52 @@ const Listing = () => {
           <div className="card flex">
             <div className="users">
               <img src={user4} alt="User Image" />
-              <img src={user5} alt="User Image" />
+
               <img src={user6} alt="User Image" />
             </div>
             <div className="cardText">
               <span>
-                656 coffee mugs <br />
+                356 coffee mugs <br />
                 <small>
                   15 sells within <span className="date">10 Hours</span>
+                </small>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="featuredSellers">
+          <div className="heading flex">
+            <h3>Featured Sellers</h3>
+            <button className="btn flex">
+              See All <BsArrowRightShort className="icon" />
+            </button>
+          </div>
+
+          <div className="card flex">
+            <div className="users">
+              <img src={user1} alt="User Image" />
+              <img src={user6} alt="User Image" />
+            </div>
+            <div className="cardText">
+              <span>
+                756 Flavoured Coffee <br />
+                <small>
+                  12 sells within <span className="date">10 Weeks</span>
+                </small>
+              </span>
+            </div>
+          </div>
+          <div className="card flex">
+            <div className="users">
+              <img src={user5} alt="User Image" />
+              <img src={user2} alt="User Image" />
+            </div>
+            <div className="cardText">
+              <span>
+                975 coffee mugs <br />
+                <small>
+                  15 sells within <span className="date">12 Days</span>
                 </small>
               </span>
             </div>
